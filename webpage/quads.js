85388aa0dc6181ce748163954607f228db816b92
ChrispyWood <woodc@uta.edu>
2021-12-20 09:10:30 -0600

    Used makeQuadTable() function to handle both table types

--- webpage/quads.js
+++ webpage/quads.js
@@ -1,18 +1,16 @@
-// create an array to hold player objects
-let players = [];
+let players = []; // an array to hold player objects
 
 // if in local storage, get it and display
-if (localStorage.getItem('players') === null) {
-  players = [];
-} else {
+if (localStorage.getItem('players') === null) players = [];
+else {
   players = JSON.parse(localStorage.getItem('players'));
   makeNewList();
 }
 
-
-/* * * * * * * * *
+/* * * * * * * * * * * * * * * * *
  * Event Listeners
- * * * * * * * * */
+ * * * * * * * * * * * * * * * * */
+
 // Add a player
 document.querySelector('#add-player').addEventListener('submit',
 function(e){
@@ -29,6 +27,7 @@ function(e){
   document.getElementById('form-first').focus();
   e.preventDefault();
 });
+// End of Add a Player
 
 // Search USCF for a player
 document.querySelector('#search-player-btn').addEventListener('click',
@@ -41,10 +40,11 @@ function(e){
   if (id.length < 1 && last.length < 1) {
     alert('You will need to provide a last name or a USCF ID.');
   } else {
-      url = "http://uschess.org/msa/thin3.php?" + id;      
+      url = "https://uschess.org/msa/thin3.php?" + id;      
   }
   document.getElementById('uscf-frame').src = url;
 });
+// End of Search USCF for a player
 
 // Remove a player
 document.querySelector('#remove-player-btn').addEventListener('click',
@@ -61,7 +61,7 @@ function(e){
     }
   }
 });
-
+// End of remove a player
 
 /* * * * * * * * * * * * * * * * * *
  * makeNewList()
@@ -78,119 +78,58 @@ function makeNewList() {
       ratingAsNumber = Number(players[i].rating);
       players[i].rating = ratingAsNumber;
     }
-  }
+  } 
 
   // Sort players by rating
   players.sort((a, b) => { return b.rating - a.rating;});
-  
-  // identify the player list and printable quad tables
+
+  // The player list and printable quad tables
   const quadList = document.querySelector('#quad-list');
   const quadTables = document.querySelector('#printable-quads');
-  // Clear the Current List and Tables
-  quadList.innerHTML = "";
-  quadTables.innerHTML = "";
-  // To check if small swiss is titled
-  let smallSwissTitle = false;
-  // quadplayer is to see if it is time to make a title
-  let quadplayer = 0;
-  // players left is how many players are yet to be grouped
-  let playersLeft = 0;
-  // quad number
-  let numOfQuads = 0; 
+  quadList.innerHTML = ""; // Clear the current player list
+  quadTables.innerHTML = ""; // Clear the printable tables
+  let smallSwissTitle = false; // To check if small swiss is titled
+  let quadplayer = 0; // quadplayer is to see if it is time to make a title
+  let playersLeft = 0; // Players left to be grouped
+  let numOfQuads = 0; // Number of the quad
 
   // Loop through array creating the Quads
   for (let i = 0; i < players.length; i++) {
     playersLeft = players.length - i;
     quadplayer = (i % 4);
+
+    // Make a title if this is a new quad or small swiss
     if (quadplayer === 0) {
-
-      // if more than 7 or exactly 4 are left, then make a quad title
       if (playersLeft > 7 || playersLeft == 4) {
-
-        // the quad number
-        numOfQuads++;
-
-        // Immediate Display List
+        // if more than 7 or exactly 4 are left, then make a quad title
+        numOfQuads++; // the quad number
         let quadTitle = document.createElement('h4');
         quadTitle.className = 'quad-title';
         quadTitle.innerHTML = `Quad ${numOfQuads}`;
         quadList.appendChild(quadTitle);
-
-        // Make the Quad Tables (quadTables)
-        let quadTable = document.createElement('table');
-        quadTable.className = 'quad-table table';
-        quadTable.id = `quad-${numOfQuads}`;
-        let quadTableTopRow = document.createElement('tr');
-        quadTableTopRow.className = 'title-row';
-        let quadTableTopCell = document.createElement('th');
-        quadTableTopCell.className = 'title-cell';
-        quadTableTopCell.innerHTML = `Quad ${numOfQuads}`;
-        quadTableTopRow.appendChild(quadTableTopCell);
-        let quadTableRoundOneCell = document.createElement('td');
-        quadTableRoundOneCell.className = 'round-title-cell';
-        quadTableRoundOneCell.innerHTML = "Rnd 1";
-        quadTableTopRow.appendChild(quadTableRoundOneCell);
-        let quadTableRoundTwoCell = document.createElement('td');
-        quadTableRoundTwoCell.className = 'round-title-cell';
-        quadTableRoundTwoCell.innerHTML = "Rnd 2";
-        quadTableTopRow.appendChild(quadTableRoundTwoCell);
-        let quadTableRoundThreeCell = document.createElement('td');
-        quadTableRoundThreeCell.className = 'round-title-cell';
-        quadTableRoundThreeCell.innerHTML = "Rnd 3";
-        quadTableTopRow.appendChild(quadTableRoundThreeCell);
-        quadTable.appendChild(quadTableTopRow);
-        quadTables.appendChild(quadTable);
-
+        makeQuadTable('quad',numOfQuads); // Make the Quad Tables (quadTables)
       } else {
         // if a small swiss title has not been made, make one
         if (smallSwissTitle === false) {
-            
-          // the quad number
-          numOfQuads++;
-
-          // Immdediate Display List
+          numOfQuads++; // the quad number
           let quadTitle = document.createElement('h4');
           quadTitle.className = 'quad-title';
           quadTitle.innerHTML = 'Small Swiss';
           quadList.appendChild(quadTitle);
           smallSwissTitle = true;
-
-          // Make the Quad Tables (quadTables)
-          let quadTable = document.createElement('table');
-          quadTable.className = 'small-swiss-table quad-table table';
-          quadTable.id = `quad-${numOfQuads}`;
-          let quadTableTopRow = document.createElement('tr');
-          quadTableTopRow.className = 'title-row';
-          let quadTableTopCell = document.createElement('th');
-          quadTableTopCell.className = 'title-cell';
-          quadTableTopCell.innerHTML = `Small Swiss`;
-          quadTableTopRow.appendChild(quadTableTopCell);
-          let quadTableRoundOneCell = document.createElement('td');
-          quadTableRoundOneCell.className = 'round-title-cell';
-          quadTableRoundOneCell.innerHTML = "Rnd 1";
-          quadTableTopRow.appendChild(quadTableRoundOneCell);
-          let quadTableRoundTwoCell = document.createElement('td');
-          quadTableRoundTwoCell.className = 'round-title-cell';
-          quadTableRoundTwoCell.innerHTML = "Rnd 2";
-          quadTableTopRow.appendChild(quadTableRoundTwoCell);
-          let quadTableRoundThreeCell = document.createElement('td');
-          quadTableRoundThreeCell.className = 'round-title-cell';
-          quadTableRoundThreeCell.innerHTML = "Rnd 3";
-          quadTableTopRow.appendChild(quadTableRoundThreeCell);
-          quadTable.appendChild(quadTableTopRow);
-          quadTables.appendChild(quadTable);
-
+          makeQuadTable('swiss',numOfQuads); // Make the Quad Tables (quadTables)
         }
       }
-    }
+    } // End of Make a title if this sia new quad or small swiss
 
-    // add the player to the list
+    // Add the player to the immediately visible list
     let quadPlayer = document.createElement('p');
     quadPlayer.className = 'quad-player';
     quadPlayer.innerHTML = players[i].first.toUpperCase() + " " + players[i].last.toUpperCase() + " (" + players[i].rating + ") " + players[i].id;
     quadList.appendChild(quadPlayer);
+    // End of Add the player to the visible list
 
-    // The Printable Quads Player Row
+    // Add the player to the printable tables 
     let quadRow = document.createElement('tr');
     quadRow.className = 'quad-row';
     quadRow.id = `player-${i}`;
@@ -198,12 +137,11 @@ function makeNewList() {
     + "<br /> &nbsp; &nbsp; &nbsp; (" + players[i].rating + ") " + players[i].id
      + "<td></td><td></td><td>color: &nbsp; W &nbsp; B</td></tr>";
     document.getElementById(`quad-${numOfQuads}`).appendChild(quadRow);
-    
+    // End of add the player to the printable tables
   }
 
   // Add instructions on tables
   const qTs = document.querySelectorAll('.quad-table');
-  
   for (let k = 0; k < qTs.length; k++) {
     let quadInstruction = "<p><strong>Round One</strong><br />"
     + "Player 1 plays white against Player 4.<br />"
@@ -217,19 +155,53 @@ function makeNewList() {
     + "Player 1 plays against Player 3 (draw for color).<br />"
     + "Player 4 plays against Player 2 (draw for color).<br />"
     + "Indicate points in table above and circle color played.</p>";
-  
     let quadInstructionRow = document.createElement("tr");
     let quadInstructionCell = document.createElement("td");
     quadInstructionCell.setAttribute("colspan", "4");
     quadInstructionCell.className = "quad-instructions";
     quadInstructionCell.innerHTML = quadInstruction;
     quadInstructionRow.appendChild(quadInstructionCell);
-
     qTs[k].appendChild(quadInstructionRow);
   }  
+  // End of Add instructions on tables loop
 
-}
-
+  // makeQuadTable() - Make the Quad or Small Swiss Table
+  function makeQuadTable(quadType,numOfQuads) {
+    let quadTable = document.createElement('table');
+    if(quadType=='swiss') {
+      quadTable.className = 'small-swiss-table quad-table table';
+    } else {
+      quadTable.className = 'quad-table table';
+    } 
+    quadTable.id = `quad-${numOfQuads}`;
+    let quadTableTopRow = document.createElement('tr');
+    quadTableTopRow.className = 'title-row';
+    let quadTableTopCell = document.createElement('th');
+    quadTableTopCell.className = 'title-cell';
+    if(quadType=='swiss') {
+      quadTableTopCell.innerHTML = `Small Swiss`;
+    } else {
+      quadTableTopCell.innerHTML = `Quad ${numOfQuads}`;
+    }
+    quadTableTopRow.appendChild(quadTableTopCell);
+    let quadTableRoundOneCell = document.createElement('td');
+    quadTableRoundOneCell.className = 'round-title-cell';
+    quadTableRoundOneCell.innerHTML = "Rnd 1";
+    quadTableTopRow.appendChild(quadTableRoundOneCell);
+    let quadTableRoundTwoCell = document.createElement('td');
+    quadTableRoundTwoCell.className = 'round-title-cell';
+    quadTableRoundTwoCell.innerHTML = "Rnd 2";
+    quadTableTopRow.appendChild(quadTableRoundTwoCell);
+    let quadTableRoundThreeCell = document.createElement('td');
+    quadTableRoundThreeCell.className = 'round-title-cell';
+    quadTableRoundThreeCell.innerHTML = "Rnd 3";
+    quadTableTopRow.appendChild(quadTableRoundThreeCell);
+    quadTable.appendChild(quadTableTopRow);
+    quadTables.appendChild(quadTable);
+  } 
+  // End of makeQuadTable() Make the Quad or Small Swiss Table
+} 
+// End of makeNewList() - Make a new player list
 
 /* * * * * * * * * * * * * * * * * *
  * clearForm()
@@ -241,5 +213,4 @@ function clearForm() {
   forEach(playerAddFormInputs, function() {
     playerAddFormInputs.value = '';
   });
-
 }
